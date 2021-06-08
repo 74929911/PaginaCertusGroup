fe83ff4477854ab6bec37b38d87ec8aae4061153
carloslorap <74977007@certus.edu.pe>
2021-06-07 22:28:09 -0500

    comint

---
+++ noticias.js
@@ -0,0 +1,278 @@
+
+window.addEventListener('load' , function () { 
+    console.log('el contenido ha cargado');
+    
+    var imagenes =[]
+    
+    imagenes[0]='carlos-ghostunner/carlos1.jpg'
+    imagenes[1]='carlos-ghostunner/carlos2.jpg'
+    imagenes[2]='carlos-ghostunner/carlos3.jpg'
+    
+    var indiceImagenes=0;
+    
+    function cambiarImagenes(){
+    
+        document.PAGINACERTUSGROUP.src=imagenes[indiceImagenes];
+    
+    if (indiceImagenes < 2) {
+        indiceImagenes++; 
+        
+    }else{
+    indiceImagenes = 0;
+    }
+    
+    }
+    
+    setInterval(cambiarImagenes,3000);
+    
+    });
+
+
+///7//7777777777777777777777777777777777777777777777777777
+
+
+    window.addEventListener('load' , function () { 
+        console.log('el contenido ha cargado');
+        
+        var imagenes =[]
+        
+        imagenes[0]='carlos-village/enrique1.jpg'
+        imagenes[1]='carlos-village/enrique2.jpg'
+        imagenes[2]='carlos-village/enrique3.jpg'
+        
+        var indceImagenes=0;
+        
+        function cambarImagenes(){
+        
+            document.PAGINACERTUSGROU.src=imagenes[indceImagenes];
+        
+        if (indceImagenes < 2) {
+            indceImagenes++; 
+            
+        }else{
+        indceImagenes = 0;
+        }
+        
+        }
+        
+        setInterval(cambarImagenes,3000);
+        
+        });
+////////////////////////////////////////////////////////////////////////////////////
+        
+
+window.addEventListener('load' , function () { 
+    console.log('el contenido ha cargado');
+    
+    var imagenes =[]
+    
+    imagenes[0]='carlos-play/play1.jpg'
+    imagenes[1]='carlos-play/play2.jpg'
+    imagenes[2]='carlos-play/play3.jpg'
+    
+    var indiceImagenes=0;
+    
+    function cambiarImagenes(){
+    
+        document.playx.src=imagenes[indiceImagenes];
+    
+    if (indiceImagenes < 2) {
+        indiceImagenes++; 
+        
+    }else{
+    indiceImagenes = 0;
+    }
+    
+    }
+    
+    setInterval(cambiarImagenes,3000);
+    
+    });
+
+/////////////////////////////////////////////////////////////////
+
+
+            window.addEventListener('load' , function () { 
+                console.log('el contenido ha cargado');
+                
+                var imagenes =[]
+                
+                imagenes[0]='daygones/day1.jpg'
+                imagenes[1]='daygones/day2.jpg'
+                imagenes[2]='daygones/day3.jpg'
+                
+                var indiceImagenes=0;
+                
+                function cambiarImagenes(){
+                
+                    document.daygone.src=imagenes[indiceImagenes];
+                
+                if (indiceImagenes < 2) {
+                    indiceImagenes++; 
+                    
+                }else{
+                indiceImagenes = 0;
+                }
+                
+                }
+                
+                setInterval(cambiarImagenes,3000);
+                
+                });
+//////////////////////////////////////////////////////////////////////
+
+window.addEventListener('load' , function () { 
+    console.log('el contenido ha cargado');
+    
+    var imagenes =[]
+    
+    imagenes[0]='revenge/re1.jpg'
+    imagenes[1]='revenge/re2.jpg'
+    imagenes[2]='revenge/re3.jpg'
+    
+    var indiceImagenes=0;
+    
+    function cambiarImagenes(){
+    
+        document.reven.src=imagenes[indiceImagenes];
+    
+    if (indiceImagenes < 2) {
+        indiceImagenes++; 
+        
+    }else{
+    indiceImagenes = 0;
+    }
+    
+    }
+    
+    setInterval(cambiarImagenes,3000);
+    
+    });
+
+//////////////////////////////////////////////////////////////
+
+
+    window.addEventListener('load' , function () { 
+        console.log('el contenido ha cargado');
+        
+        var imagenes =[]
+        
+        imagenes[0]='hoizon/ori1.jpg'
+        imagenes[1]='hoizon/ori2.jpg'
+        imagenes[2]='hoizon/ori3.jpg'
+        
+        var indiceImagenes=0;
+        
+        function cambiarImagenes(){
+        
+            document.Horizon.src=imagenes[indiceImagenes];
+        
+        if (indiceImagenes < 2) {
+            indiceImagenes++; 
+            
+        }else{
+        indiceImagenes = 0;
+        }
+        
+        }
+        
+        setInterval(cambiarImagenes,3000);
+        
+        });
+
+
+////////////////////////////////////////////////////
+
+
+
+window.addEventListener('load' , function () { 
+    console.log('el contenido ha cargado');
+    
+    var imagenes =[]
+    
+    imagenes[0]='fornite/for1.jpg'
+    imagenes[1]='fornite/for2.jpg'
+    imagenes[2]='fornite/for3.jpg'
+    
+    var indiceImagenes=0;
+    
+    function cambiarImagenes(){
+    
+        document.forti.src=imagenes[indiceImagenes];
+    
+    if (indiceImagenes < 2) {
+        indiceImagenes++; 
+        
+    }else{
+    indiceImagenes = 0;
+    }
+    
+    }
+    
+    setInterval(cambiarImagenes,3000);
+    
+    });
+
+
+
+///////////////////////////////////////////////////////////////////
+
+
+window.addEventListener('load' , function () { 
+    console.log('el contenido ha cargado');
+    
+    var imagenes =[]
+    
+    imagenes[0]='gta/g1.jpg' 
+    imagenes[1]='gta/g2.jpg' 
+    imagenes[2]='gta/g3.jpg'
+    
+    var indiceImagenes=0;
+    
+    function cambiarImagenes(){
+    
+        document.vite.src=imagenes[indiceImagenes];
+    
+    if (indiceImagenes < 2) {
+        indiceImagenes++; 
+        
+    }else{
+    indiceImagenes = 0;
+    }
+    
+    }
+    
+    setInterval(cambiarImagenes,3000);
+    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
